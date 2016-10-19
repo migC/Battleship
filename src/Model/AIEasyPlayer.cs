852f4d894f02a0697fa99b03da64f76480ca3ebe
hirustaa23 <hirustaaa@gmail.com>
2016-10-19 17:16:43 +1100

    final fix for easy ai player and ai buttons

--- src/Model/AIEasyPlayer.cs
+++ src/Model/AIEasyPlayer.cs
@@ -38,9 +38,3 @@ class AIEasyPlayer : AIPlayer
 }
 
 
-//=======================================================
-//Service provided by Telerik (www.telerik.com)
-//Conversion powered by NRefactory.
-//Twitter: @telerik
-//Facebook: facebook.com/telerik
-//=======================================================
